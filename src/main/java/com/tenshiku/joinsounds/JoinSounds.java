5556703479753f6e3fa2d9255ffd650b511514de
Noah <youthinrevoltmc@outlook.com>
2025-07-22 17:24:44 +1000

    Initial commit

---
+++ src/main/java/com/tenshiku/joinsounds/JoinSounds.java
@@ -0,0 +1,17 @@
+package com.tenshiku.joinsounds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JoinSounds extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
